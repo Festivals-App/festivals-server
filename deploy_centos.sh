3a6237c8bc1d8d0e2edfef5d5de1cb515c1e8a24
Simon Gaus <phisto05@gmail.com>
2020-10-23 02:14:06 +0200

    Add script description printing

--- deploy_centos.sh
+++ deploy_centos.sh
@@ -4,29 +4,36 @@
 
 systemctl enable firewalld >/dev/null
 systemctl start firewalld >/dev/null
-echo "Enabled firewalld"
+echo "1. Enabled firewalld"
 
 firewall-cmd --permanent --new-service=festivals-server
 firewall-cmd --permanent --service=festivals-server --set-description="A live and lightweight go server app providing the FestivalsAPI."
 firewall-cmd --permanent --service=festivals-server --add-port=10439/tcp
 firewall-cmd --permanent --add-service=festivals-server
 firewall-cmd --reload
-echo "Add festivals-server.service to firewalld"
+echo "2. Add festivals-server.service to firewalld"
 
 curl -o go.tar.gz "https://dl.google.com/go/$(curl "https://golang.org/VERSION?m=text").linux-amd64.tar.gz"
 tar -C /usr/local -xf go.tar.gz
 rm go.tar.gz
 ln -s /usr/local/go/bin/* /usr/local/bin
-echo "Installed go"
+echo "3. Installed go"
 
 # install repository
-dnf install git --assumeyes
-go get github.com/Festivals-App/festivals-server
+dnf install git --assumeyes >/dev/null
+echo "4. Installed git"
+
+go get github.com/Festivals-App/festivals-server >/dev/null
+echo "5. Downloaded festivals-server"
+
 cd ~/go/src/github.com/Festivals-App/festivals-server || exit
 go build main.go
+echo "6. Build festivals-server"
+
 mv main /usr/local/bin/festivals-server
 restorecon -v /usr/local/bin/festivals-server
 mv config_template.toml /etc/festivals-server.conf
+echo "7. Installed festivals-server"
 
 # create systemctl service
 sudo tee -a /etc/systemd/system/festivals-server.service > /dev/null <<EOT
@@ -44,12 +51,15 @@ ExecStart=/usr/local/bin/festivals-server
 [Install]
 WantedBy=multi-user.target
 EOT
+echo "8. Created systemd service"
 
 systemctl enable festivals-server
 systemctl start festivals-server
+echo "9. Enabled systemd service"
 
 # cleanup after installation
 rm -R ~/go
+
 
 # remove this script
 rm -- "$0"
