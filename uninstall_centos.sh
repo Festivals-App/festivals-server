c94f17a6dc855c15f1316269b919b7e4de0240c7
Simon Gaus <phisto05@gmail.com>
2020-10-24 12:02:51 +0200

    Create uninstall script

---
+++ uninstall_centos.sh
@@ -0,0 +1,18 @@
+firewall-cmd --permanent --remove-service=festivals-server
+
+firewall-cmd --permanent --remove-service=festivals-server
+rm -f /etc/firewalld/services/festivals-server.xml
+rm -f /etc/firewalld/services/festivals-server.xml.old
+firewall-cmd --reload
+firewall-cmd --info-service=festivals-server
+
+rm -R /usr/local/go
+rm /usr/local/bin/festivals-server
+rm -R /var/log/festivals-server
+rm /usr/local/bin/go
+rm /usr/local/bin/gofmt
+
+dnf remove unzip --assumeyes
+
+systemctl disable festivals-server
+rm /etc/systemd/system/festivals-server.service
