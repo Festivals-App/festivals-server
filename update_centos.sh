50ef39d95a329169d383626523c14a71e6d068c8
Simon Gaus <phisto05@gmail.com>
2020-10-24 03:50:41 +0200

    Update update script

--- update_centos.sh
+++ update_centos.sh
@@ -7,30 +7,35 @@ cd /usr/local
 systemctl stop festivals-server
 echo "1. Stopped festivals-server"
 
-curl -o /usr/local/go.tar.gz "https://dl.google.com/go/$(curl "https://golang.org/VERSION?m=text").linux-amd64.tar.gz" >/dev/null
+echo "2. Downloading current go version"
+curl -o /usr/local/go.tar.gz "https://dl.google.com/go/$(curl --silent "https://golang.org/VERSION?m=text").linux-amd64.tar.gz"
 tar -C /usr/local -xf /usr/local/go.tar.gz
 rm /usr/local/go.tar.gz
 ln -sf /usr/local/go/bin/* /usr/local/bin
-echo "2. Updated go"
+echo "3. Updated go"
 
-echo "3. Download current festivals-server"
+echo "4. Download current festivals-server"
 curl -L -o /usr/local/festivals-server.zip https://github.com/Festivals-App/festivals-server/archive/master.zip >/dev/null
 unzip /usr/local/festivals-server.zip -d /usr/local >/dev/null
 rm /usr/local/festivals-server.zip
-echo "4. Downloaded current festivals-server"
+echo "5. Downloaded current festivals-server"
+sleep 1
 
 cd /usr/local/festivals-server-master || exit
 /usr/local/bin/go build main.go
-echo "5. Build festivals-server"
+echo "6. Build festivals-server"
+sleep 1
 
 mv main /usr/local/bin/festivals-server
 restorecon -v /usr/local/bin/festivals-server
-echo "6. Installed festivals-server"
+echo "7. Installed festivals-server"
+sleep 1
 
 systemctl start festivals-server
-echo "7. Enabled systemd service"
+echo "8. Enabled systemd service"
+sleep 1
 
 cd /usr/local
 rm -R /usr/local/festivals-server-master
-echo "8. Cleaning up after updating"
-sleep 2
+echo "9. Cleaning up after updating"
+sleep 1
