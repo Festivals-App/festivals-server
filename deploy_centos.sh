5701514ff4af931cb8ff9a484e299ddcfd43383d
Simon Gaus <phisto05@gmail.com>
2020-10-24 13:07:30 +0200

    Add pauses

--- deploy_centos.sh
+++ deploy_centos.sh
@@ -14,6 +14,7 @@ firewall-cmd --permanent --service=festivals-server --add-port=10439/tcp >/dev/n
 firewall-cmd --permanent --add-service=festivals-server >/dev/null
 firewall-cmd --reload >/dev/null
 echo "2. Added festivals-server.service to firewalld"
+sleep 1
 
 echo "2. Downloading current go version..."
 curl -o /usr/local/go.tar.gz "https://dl.google.com/go/$(curl --silent "https://golang.org/VERSION?m=text").linux-amd64.tar.gz"
@@ -26,6 +27,7 @@ sleep 1
 echo "4. Downloading unzip..."
 dnf install unzip --assumeyes >/dev/null
 echo "5. Installed unzip"
+sleep 1
 
 echo "6. Downloading current festivals-server..."
 curl -L -o /usr/local/festivals-server.zip https://github.com/Festivals-App/festivals-server/archive/master.zip
@@ -40,6 +42,7 @@ mv main /usr/local/bin/festivals-server
 restorecon -v /usr/local/bin/festivals-server >/dev/null
 mv config_template.toml /etc/festivals-server.conf
 echo "8. Installed festivals-server"
+sleep 1
 
 # create systemctl service
 sudo tee -a /etc/systemd/system/festivals-server.service > /dev/null <<EOT
@@ -58,10 +61,12 @@ ExecStart=/usr/local/bin/festivals-server
 WantedBy=multi-user.target
 EOT
 echo "9. Created systemd service"
+sleep 1
 
 systemctl enable festivals-server
 systemctl start festivals-server
 echo "10. Enabled systemd service"
+sleep 1
 
 rm -R /usr/local/festivals-server-master
 echo "11. Done"
